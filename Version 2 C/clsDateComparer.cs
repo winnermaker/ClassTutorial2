388c47e17547da60da28231fc851f26842764a87
MatthiasNZ <matthias.otto@nmit.ac.nz>
2017-03-27 15:37:59 +1300

    Clarified comparer versions (i.e. clsDateComparer, clsDDateComparer)

--- Version 2 C/clsDateComparer.cs
+++ Version 2 C/clsDateComparer.cs
@@ -7,25 +7,22 @@ namespace Version_2_C
     {
         public int Compare(clsWork x, clsWork y)
         {
+            // long version:
             //DateTime lcDateX = x.Date;
             //DateTime lcDateY = y.Date;
-
-            return x.Date.CompareTo(y.Date);
-
             //return lcDateX.CompareTo(lcDateY);
+
+            // short version:
+            return x.Date.CompareTo(y.Date);
         }
     }
 
     class clsDDateComparer : IComparer<clsWork>
+    // descending date comparer, i.e. reverse sorting order!
     {
         public int Compare(clsWork x, clsWork y)
         {
-            //DateTime lcDateX = x.Date;
-            //DateTime lcDateY = y.Date;
-
             return y.Date.CompareTo(x.Date);
-
-            //return lcDateX.CompareTo(lcDateY);
         }
     }
 }
